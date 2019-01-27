be7606664cdeac95bc62d10136130e5db25dd73d
Alex <gurkslask@gmail.com>
2019-01-27 21:01:51 +0100

    Some modifications to make it work with systemd

--- web/main.go
+++ web/main.go
@@ -4,14 +4,46 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"strings"
+
+	"github.com/gurkslask/AC500Convert"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("index.html")
+	t, err := template.ParseFiles("./web/index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
 	t.Execute(w, nil)
+}
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	in := r.FormValue("body")
+
+	stext := strings.Split(in, "\n")
+	vars, err := AC500Convert.ExtractData(stext)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	s := AC500Convert.OutputToText(vars)
+
+	i := &info{Text: s}
+	t, err := template.ParseFiles("./web/view.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = t.Execute(w, i)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/upload", uploadHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+type info struct {
+	Text string
 }
